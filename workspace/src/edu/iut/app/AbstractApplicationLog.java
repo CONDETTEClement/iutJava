1ca181d052f465a60b1b153f35908495bd21cf06
CONDETTEClement <clement.condette@free.fr>
2015-11-10 14:14:40 +0100

    modif du répertoire workspace (tp1)

--- workspace/src/edu/iut/app/AbstractApplicationLog.java
+++ workspace/src/edu/iut/app/AbstractApplicationLog.java
@@ -6,15 +6,28 @@ public abstract class AbstractApplicationLog implements IApplicationLog {
 
 	protected String message;
 	/** TP1 : Tableau au sens des collections **/
-	protected /*TYPE TABLEAU*/ listeners;
+	protected ArrayList<IApplicationLogListener> listeners;
 	
 	public AbstractApplicationLog() {
 		message = null;
-		listeners = new /*TYPE TABLEAU*/();
+		listeners = new ArrayList<IApplicationLogListener>();
 	}
 	
     /** TP1 : Fonction venant de l'interface par héritage */
+	public abstract void setMessage(String message);
 
+	public String getMessage() {
+		return message;
+	}
+
+	public void addListener(IApplicationLogListener listener) {
+		listeners.add(listener);
+
+	}
+	public IApplicationLogListener[] getApplicationLogListeners() {
+		return (IApplicationLogListener[])listeners.toArray();
+	}
+	
 
 	/** Listener action */
 	protected void fireMessage(String level, String message) {
